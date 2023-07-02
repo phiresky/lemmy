dd4b9091d592e7dcde79f4ff17265d34dd8fe2c9
phiresky <phireskyde+git@gmail.com>
2023-07-02 10:41:54 +0000

    use atty to disable color if outputting to file

--- src/lib.rs
+++ src/lib.rs
@@ -190,7 +190,9 @@ pub fn init_logging(opentelemetry_url: &Option<Url>) -> Result<(), LemmyError> {
     .trim_matches('"')
     .parse::<Targets>()?;
 
-  let format_layer = tracing_subscriber::fmt::layer().with_filter(targets.clone());
+  let format_layer = tracing_subscriber::fmt::layer()
+    .with_ansi(atty::is(atty::Stream::Stdout))
+    .with_filter(targets.clone());
 
   let subscriber = Registry::default()
     .with(format_layer)
